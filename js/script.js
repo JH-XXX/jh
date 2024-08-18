a8633243dfd5227f794c7509a8473cb5f4bb2a94
JXHXX <998715@naver.com>
2024-08-18 15:51:27 +0900

    scroll

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@ let lastScroll = 0;
 
 
 
-// window.addEventListener('scroll',()=>{
-//   let currentScroll = window.scrollY;
+window.addEventListener('scroll',()=>{
+  let currentScroll = window.scrollY;
 
-//   if(currentScroll > lastScroll){
-//     body.classList.remove('scroll-up')
-//     body.classList.add('scroll-down')
-//   }else if(currentScroll < lastScroll){
-//     body.classList.add('scroll-up')
-//     body.classList.remove('scroll-down')
-//   }
-//   lastScroll = currentScroll;
-// });
+  if(currentScroll > lastScroll){
+    body.classList.remove('scroll-up')
+    body.classList.add('scroll-down')
+  }else if(currentScroll < lastScroll){
+    body.classList.add('scroll-up')
+    body.classList.remove('scroll-down')
+  }
+  lastScroll = currentScroll;
+});
